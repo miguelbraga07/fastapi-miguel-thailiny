fe83888b792990272805291ca0927dd892ab9e59
thailinyanjos <thailiny.anjos05@aluno.ifce.edu.br>
2025-08-25 13:58:34 +0000

    alteraçao

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI(title='API do Miguel e da Thailiny')
 @app.get("/")
 def hello():
     return{"message":"Hello World!"}
+
+@app.get("/aluno")
+def get_aluno():
+    return{"insira na rota após a '/' o nome do aluno"}
+
+@app.get("/aluno/{nome_aluno}")
+def get_aluno_by_name(nome_aluno: str): 
+    return{"o nome do aluno é": nome_aluno}
